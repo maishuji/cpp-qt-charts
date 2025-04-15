81112e1c339034154581b00f27863319ba191acc
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-04-15 21:01:33 +0200

    Add menu and start versioning

---
+++ src/MainWindow.cpp
@@ -0,0 +1,42 @@
+// mainwindow.cpp
+#include "MainWindow.hpp"
+
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QInputDialog>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+
+#include "version.hpp"
+#include "ChartWindow.hpp"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    chartWindow = new ChartWindow(this); // reuse your existing QWidget
+    setCentralWidget(chartWindow);       // embed it in the main window
+
+    // Create Menu
+    menuBar = new QMenuBar(this);
+    fileMenu = new QMenu("File", this);
+    exitAction = new QAction("Exit", this);
+    menuBar->addMenu(fileMenu);
+
+    helpMenu = new QMenu("Help", this);
+    versionAction = new QAction("Version", this);
+    helpMenu->addAction(versionAction);
+    menuBar->addMenu(helpMenu);
+
+    setMenuBar(menuBar);
+
+    connect(versionAction, &QAction::triggered, this, &MainWindow::showVersion);
+    connect(exitAction, &QAction::triggered, this, &MainWindow::close);
+}
+
+void MainWindow::showVersion()
+{
+    // Show version using a message box
+    QMessageBox::information(this, tr("Version Information"),
+                             tr("Version: %1").arg(APP_VERSION_STRING));
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <QApplication>
+#include "MainWindow.hpp"
 #include "ChartWindow.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    ChartWindow window;
+
+    MainWindow window;
     window.show();
 
     return app.exec();
