4a8d9ebc71bab1116a73ffcc9eceb119f3c1a931
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-04-29 22:54:27 +0200

    Move menu items to pimpl

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -11,25 +11,44 @@
 #include "ChartWindow.hpp"
 #include "version.hpp"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+struct MainWindow::PImpl {
+    QAction *exitAction = nullptr;
+    QAction *versionAction = nullptr;
+    QMenu *fileMenu = nullptr;
+    QMenu *helpMenu = nullptr;
+};
+
+MainWindow::~MainWindow() = default;  // Destructor definition
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), m_pimpl(std::make_unique<PImpl>()) {
     chartWindow = new ChartWindow(this);  //  reuse your existing QWidget
     setCentralWidget(chartWindow);        //  embed it in the main window
 
     // Create Menu
     menuBar = new QMenuBar(this);
-    fileMenu = new QMenu("File", this);
-    exitAction = new QAction("Exit", this);
-    menuBar->addMenu(fileMenu);
 
-    helpMenu = new QMenu("Help", this);
-    versionAction = new QAction("Version", this);
-    helpMenu->addAction(versionAction);
-    menuBar->addMenu(helpMenu);
+    //  File Menu
+    m_pimpl->fileMenu = new QMenu("File", this);
+    m_pimpl->exitAction = new QAction("Exit", this);
+    m_pimpl->fileMenu->addAction(m_pimpl->exitAction);
+    m_pimpl->exitAction->setShortcut(QKeySequence::Quit);
+
+    menuBar->addMenu(m_pimpl->fileMenu);
+
+    //  Help Menu
+    m_pimpl->helpMenu = new QMenu("Help", this);
+    m_pimpl->versionAction = new QAction("Version", this);
+    m_pimpl->helpMenu->addAction(m_pimpl->versionAction);
+    menuBar->addMenu(m_pimpl->helpMenu);
 
     setMenuBar(menuBar);
 
-    connect(versionAction, &QAction::triggered, this, &MainWindow::showVersion);
-    connect(exitAction, &QAction::triggered, this, &MainWindow::close);
+    connect(m_pimpl->versionAction,
+            &QAction::triggered,
+            this,
+            &MainWindow::showVersion);
+    connect(m_pimpl->exitAction, &QAction::triggered, this, &MainWindow::close);
 }
 
 void MainWindow::showVersion() {
